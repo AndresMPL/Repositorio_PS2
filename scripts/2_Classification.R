46f121520bec09492509503424e516e5576ae668
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-02-21 17:43:08 -0500

    Create 2_Classification.R

---
+++ scripts/2_Classification.R
@@ -0,0 +1,66 @@
+
+#------------------------------------------------------------------------------#
+#
+#                         2. POVERTY CLASSIFICATION
+#
+#------------------------------------------------------------------------------#
+
+#Modelo de Clasificación--------------------------------------------------------
+#Enfoque de clasificación - Intento de predecir directamente 0 (no pobre) y 1 (pobre)
+
+  #Mutamos factores
+  
+  #create Data Partition
+
+  
+##Evaluación Desbalance de clases-----
+
+  #Evaluación
+  
+  #Selección de variables
+  
+  #Dummyficamos
+  
+  #Dividimos train/test (70/30)
+  
+  #Estandarizamos
+  
+  #Evaluamos modelos In Sample - Out of Sample
+  
+      #Normal
+  
+      #Upsampling
+  
+      #Oversampling
+  
+      #Downsampling
+  
+      #Threshold óptimo
+  
+      #Cambiar función de costo
+  
+
+##Logit----
+
+    #fiveStats 
+  
+    #trainControl
+  
+    #mylogit_caret - TEST
+  
+    #predictTest_logit - TRAIN
+  
+  
+##LDA - Caret----
+
+  #Generamos modelo - TRAIN
+  
+  #Generamos predicciones - TEST
+  
+
+##ROC performance----
+
+  #In-sample (Logit, LDA)
+  
+  #Out-of-sample (Logit, LDA)
+
