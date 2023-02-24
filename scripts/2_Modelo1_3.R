5304cc8a6f13ccd5ded18bcf7d8780163d1cf8d0
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-02-23 22:23:15 -0500

    Resample 3 - Modelo 1

---
+++ scripts/2_Modelo1_3.R
@@ -0,0 +1,56 @@
+
+##1.3 Logit sin regularizar - Downsampling ----
+
+
+train_hhs$Pobre <- factor(train_hhs$Pobre)
+
+train_hhs13 <- downSample(x = select(train_hhs, -Pobre), 
+                          y = train_hhs$Pobre, list = F, yname = "Pobre")
+
+prop.table(table(train_hhs$Pobre)) #BD inicial
+nrow(train_hhs) 
+
+prop.table(table(train_hhs13$Pobre)) #BD remuestreo - Verificamos proporciones de cada clase
+nrow(train_hhs13) 
+
+train_hhs13 <- data.frame(sapply(train_hhs13, as.numeric))
+
+Y13 <- train_hhs13$Pobre
+X13 <- model.matrix(~, train_hhs13)
+
+modelo13 <- train(y = Y13,
+                  x = X13,
+                  data = train_hhs13, 
+                  method = "glm",
+                  trControl = ctrl,
+                  family = "binomial", 
+                  metric = 'Accuracy')
+
+y_hat_train13  <- predict(modelo13, train_hhs)
+y_hat_test13   <- predict(modelo13, test_hhs)
+y_hat_eval13   <- predict(modelo13, eval_hhs)
+
+acc_train13  <- Accuracy(y_pred = y_hat_train13, y_true = train_hhs$Pobre)
+acc_test13   <- Accuracy(y_pred = y_hat_test13, y_true = test_hhs$Pobre)
+acc_eval13   <- Accuracy(y_pred = y_hat_eval13, y_true = eval_hhs$Pobre)
+
+metricas_train13 <- data.frame(Modelo = "Logit - Down", 
+                               "Muestreo" = "Downsampling", 
+                               "Evaluación" = "Entrenamiento",
+                               "Accuracy" = acc_train13)
+
+metricas_test13 <- data.frame(Modelo = "Logit - Down", 
+                              "Muestreo" = "Downsampling", 
+                              "Evaluación" = "Test",
+                              "Accuracy" = acc_test13)
+
+metricas_eval13 <- data.frame(Modelo = "Logit - Down", 
+                              "Muestreo" = "Downsampling", 
+                              "Evaluación" = "Evaluación",
+                              "Accuracy" = acc_eval13)
+
+metricas13 <- bind_rows(metricas_train13, metricas_test13, metricas_eval13)
+metricas <- bind_rows(metricas, metricas13)
+metricas %>% kbl(digits = 2) %>% kable_styling(full_width = T)
+
+
