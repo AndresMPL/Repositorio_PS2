2927547f4201ff51c6bd05121ddcfa98f528da44
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-02-24 14:35:28 -0500

    Update 4_Comparison.R

--- scripts/4_Comparison.R
+++ scripts/4_Comparison.R
@@ -15,10 +15,13 @@
 
 
 #Generación del Modelo de Entrega-----------------------------------------------
-
+#Ejemplo Archivo "sample_submission.csv"
+  
+  
   best_model <- 
   
   poverty <- predict(best_model, test_hogares)
+  modelo_final <- poverty %>% select(id, Pobre_1)
+  write.csv(modelo_final, "modelo_final.csv")
 
 
-#Ejemplo Archivo "sample_submission.csv"
