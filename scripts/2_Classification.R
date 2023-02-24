54d9b3ed95d4483de4de47df5e4643b0e917868d
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-02-24 17:14:51 -0500

    Update 2_Classification.R

--- scripts/2_Classification.R
+++ scripts/2_Classification.R
@@ -9,10 +9,10 @@
 
 #Pasos previos ----------------------------------------------------------------
 
-  prop.table(table(train_h$Pobre_Pobre)) #1-Pobre, 0-No Pobre
-  prop.table(table(train_h$Clase_Rural))
+  prop.table(table(train_h$Pobre)) #1-Pobre, 0-No Pobre
+  prop.table(table(train_h$Clase))
   
-  train_h <- train_h %>% select(-id, -Nper, -Lp,-Ingtotugarr, -Ingpcug, -num_oc_hogar, -Pobre_No_Pobre, -Clase_Urbano, -Vivienda_Propia_paga, -sexo_jefe_hogar_Hombre, -nivel_edu_jefe_hogar_Ninguno, -jefe_hogar_des_No, -jefe_hogar_oc_No, -jefe_hogar_ina_No, -Hacinamiento_No, -jefe_hogar_oc_Si)
+  #train_h <- train_h %>% select(-id, -Nper, -Lp,-Ingtotugarr, -Ingpcug, -num_oc_hogar, -Pobre_No_Pobre, -Clase_Urbano, -Vivienda_Propia_paga, -sexo_jefe_hogar_Hombre, -nivel_edu_jefe_hogar_Ninguno, -jefe_hogar_des_No, -jefe_hogar_oc_No, -jefe_hogar_ina_No, -Hacinamiento_No, -jefe_hogar_oc_Si)
   
   #train_h <- train_hd
   
