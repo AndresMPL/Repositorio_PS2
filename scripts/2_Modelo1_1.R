8572c224e6020f017dee6cf82387edb5f108cf19
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-02-24 19:15:34 -0500

    Update 2_Modelo1_1.R

--- scripts/2_Modelo1_1.R
+++ scripts/2_Modelo1_1.R
@@ -3,8 +3,9 @@
 
       train_hhs$Pobre <- factor(train_hhs$Pobre)
       
-      train_hhs11 <- upSample(x = select(train_hhs, -Pobre), 
-                              y = train_hhs$Pobre, list = F, yname = "Pobre")
+      set.seed(10110)
+      train_hhs11 <- upSample(x = train_hhs, 
+                              y = train_hhs$Pobre, yname = "Pobre")
       
       prop.table(table(train_hhs$Pobre)) #BD inicial
       nrow(train_hhs) 
@@ -12,26 +13,40 @@
       prop.table(table(train_hhs11$Pobre)) #BD remuestreo - Verificamos proporciones de cada clase
       nrow(train_hhs11) 
       
-      train_hhs11$Pobre <- as.numeric(train_hhs11$Pobre) - 1
-
+      glimpse(train_hhs11) 
+      
+      #train_hhs11$Pobre <- as.numeric(train_hhs$Pobre)
+      #train_hhs11$Pobre <- as.numeric(train_hhs$Pobre)
+      #train_hhs11$Pobre <- as.numeric(train_hhs$Pobre)
+      
+      set.seed(10110)
       modelo11 <- train(Pobre~., 
                         data = train_hhs11,
                         method = "glmnet",
+                        trControl = control,
                         family = "binomial",
                         preProcess = NULL,
                         metric = 'Accuracy')
 
-      probs_train11  <- predict(modelo11, newdata = train_hhs, type = "prob")
-      probs_test11   <- predict(modelo11, newdata = test_hhs, type = "prob")
-      probs_eval11   <- predict(modelo11, newdata = eval_hhs, type = "prob")
+      probs_train11  <- predict(modelo11, newdata = train_hhs, type = "prob")[, 1, drop = T]
+      #probs_train11[probs_train11 < 0] <- 0
+      #probs_train11[probs_train11 > 1] <- 1
+      
+      probs_test11   <- predict(modelo11, newdata = test_hhs, type = "prob")[, 1, drop = T]
+      #probs_test11[probs_train11 < 0] <- 0
+      #probs_test11[probs_train11 > 1] <- 1
+      
+      probs_eval11   <- predict(modelo11, newdata = eval_hhs, type = "prob")[, 1, drop = T]
+      #probs_eval11[probs_train11 < 0] <- 0
+      #probs_eval11[probs_train11 > 1] <- 1
       
       y_hat_train11  <- as.numeric(probs_train11 > 0.5)
       y_hat_test11   <- as.numeric(probs_test11 > 0.5)
       y_hat_eval11   <- as.numeric(probs_eval11 > 0.5)
       
-      acc_train11  <- Accuracy(y_pred = y_hat_train11, y_true = train_hhs$Pobre)
-      acc_test11   <- Accuracy(y_pred = y_hat_test11, y_true = test_hhs$Pobre)
-      acc_eval11   <- Accuracy(y_pred = y_hat_eval11, y_true = eval_hhs$Pobre)
+      acc_train11  <- Accuracy(y_pred = y_hat_train11, y_true = as.numeric(train_hhs$Pobre))
+      acc_test11   <- Accuracy(y_pred = y_hat_test11, y_true = as.numeric(test_hhs$Pobre))
+      acc_eval11   <- Accuracy(y_pred = y_hat_eval11, y_true = as.numeric(eval_hhs$Pobre))
       
       metricas_train11 <- data.frame(Modelo = "Logit - Up", 
                                     "Muestreo" = "Upsampling", 
