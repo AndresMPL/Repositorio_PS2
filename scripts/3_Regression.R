c016ffdd3fe59b958feb0c3a16de4960b57dc5fb
manuelaojeda <m.ojeda@uniandes.edu.co>
2023-02-25 12:30:49 -0500

    Update 3_Regression.R

--- scripts/3_Regression.R
+++ scripts/3_Regression.R
@@ -9,78 +9,15 @@
 
          #PROPUESTA ESTRUCTURA PRELIMINAR
   
-#Regresión lineal---------------------------------------------------------------
+train_hp3 <- train_hp3 %>% select(-id, -Ingtotugarr, -num_oc_hogar, -Pobre_No_Pobre, -Pobre_Pobre, -Clase_Urbano, -Vivienda_Propia_paga, -sexo_jefe_hogar_Hombre, -nivel_edu_jefe_hogar_Ninguno, -jefe_hogar_des_No, -jefe_hogar_oc_No, -jefe_hogar_ina_No, -Hacinamiento_No, -jefe_hogar_oc_Si)
 
 
+#Dividimos train/test/eval (70/20/10) - BD Hogares
 
-#Control------------------------------------------------------------------------
-
-grilla2 <- 10^seq(10, -1, length = 100)
-
-fiveStats2 <- function(...) c(twoClassSummary(...), defaultSummary(...))
-
-control2 <- trainControl(method = "cv",
-                         number = 5,
-                         summaryFunction = fiveStats2,
-                         classProbs = TRUE,
-                         verbose=FALSE,
-                         savePredictions = T)
-
-
-
-#Ridge--------------------------------------------------------------------------
-
-ridge_incomes <- train(Pobre_1~P5000+P5010+Nper+Npersug+Lp+Ingtotugarr+Ingpcug+P5090_1+
-                         P5090_2+P5090_3+P5090_4+P5090_5+P5090_6+Clase_1, 
-                       data = train_hhs,
-                       method = "glmnet",
-                       trControl = control2,
-                       family = "binomial",
-                       preProcess = NULL,
-                       metric = 'Accuracy',
-                       tuneGrid = expand.grid(alpha = 0,lambda=grilla2))
-
-ridge_incomes
-
-
-#Lasso--------------------------------------------------------------------------
-
-lasso_incomes <- train(Pobre_1~P5000+P5010+Nper+Npersug+Lp+Ingtotugarr+Ingpcug+P5090_1+
-                         P5090_2+P5090_3+P5090_4+P5090_5+P5090_6+Clase_1, 
-                       data = train_hhs,
-                       method = "glmnet",
-                       trControl = control2,
-                       family = "binomial",
-                       preProcess = NULL,
-                       metric = 'Accuracy',
-                       tuneGrid = expand.grid(alpha = 1,lambda=grilla2))
-
-lasso_incomes
-
-
-
-#EN-----------------------------------------------------------------------------
-
-EN_incomes <- train(Pobre_1~P5000+P5010+Nper+Npersug+Lp+Ingtotugarr+Ingpcug+P5090_1+
-                      P5090_2+P5090_3+P5090_4+P5090_5+P5090_6+Clase_1, 
-                    data = train_hhs,
-                    method = "glmnet",
-                    trControl = control2,
-                    family = "binomial",
-                    preProcess = NULL,
-                    metric = 'Accuracy',
-                    tuneGrid = tuneGrid = expand.grid(alpha = seq(0,1,by = 0.1),lambda=grilla2))
-
-EN_incomes
-
------------------------------------------------------------------------------
-  
-  #Dividimos train/test/eval (70/20/10) - BD Hogares
-  
-  set.seed(10110)
-index_3 <- createDataPartition(y = train_h$Ingpcug , p = 0.7)[[1]]
-trainhh2<- train_h[index_3,]
-other2 <- train_h[-index_3,]
+set.seed(10110)
+index_3 <- createDataPartition(y = train_hp3$Ingpcug , p = 0.7)[[1]]
+trainhh2<- train_hp3[index_3,]
+other2 <- train_hp3[-index_3,]
 
 
 set.seed(10110)
@@ -89,12 +26,12 @@ testhh2 <- other2[index_4,]
 evalhh2 <- other2[-index_4,]
 
 
-dim(train_h)   
+dim(train_hp3)   
 dim(trainhh2)
 dim(testhh2)
 dim(evalhh2)
 
-dim(train_h)[1] - dim(trainhh2)[1] - dim(testhh2)[1] - dim(evalhh2)[1]
+dim(train_hp3)[1] - dim(trainhh2)[1] - dim(testhh2)[1] - dim(evalhh2)[1]
 
 ##Estandarizamos
 
@@ -114,6 +51,7 @@ train_hhs2[, variables_numericas] <- predict(escalador, trainhh2[, variables_num
 test_hhs2[, variables_numericas] <- predict(escalador, testhh2[, variables_numericas])
 eval_hhs2[, variables_numericas] <- predict(escalador, evalhh2[, variables_numericas])  
 
+
 #Control------------------------------------------------------------------------
 
 grilla2 <- 10^seq(10, -1, length = 100)
@@ -121,69 +59,27 @@ grilla2 <- 10^seq(10, -1, length = 100)
 fiveStats2 <- function(...) c(twoClassSummary(...), defaultSummary(...))
 
 control2 <- trainControl(method = "cv",
-                         number = 5,
-                         summaryFunction = fiveStats2,
-                         classProbs = TRUE,
-                         verbose=FALSE,
-                         savePredictions = T)
+                         number = 10)
 
-#1 - Logit 2 ---------------------------------------------------------
+###############################################################################################
 
+glimpse(train_hhs2)
+colnames(train_hhs2)
 set.seed(10110)
-modelo2 <-   train(Ingpcug ~Nper+Npersug+num_cuartos+Vivienda_Propia_paga+sexo_jefe_hogar_Mujer, 
-                   data = trainhh2,
-                   method = "lm",
-                   trControl = control2,
-                   metric = 'Roc')
-modelo2
+lineal_p3<-   train(Ingpcug ~ ., 
+                    data = train_hhs2,
+                    method = "lm",
+                    trControl = control2,
+                    preProcess = NULL)
+lineal_p3
 
-#1 - Ridge 2 ---------------------------------------------------------
+##RIDGE
+ridge_p3<-train(Ingpcug ~ .,
+                data=train_hhs2,
+                method = 'glmnet', 
+                trControl = control2,
+                tuneGrid = expand.grid(alpha = 0, lambda = grilla2)) 
+ridge_p3
 
-p_load("glmnet")
-y<-trainhh2$Ingpcug
-X<-model.matrix(~Nper+Npersug+num_cuartos+Vivienda_Propia_paga+sexo_jefe_hogar_Mujer-1,trainhh2)
-head(X)
-
-grid=10^seq(10,-2,length=100)
-grid
-
-ridge2<-glmnet(x=X,
-               y=y,
-               alpha=0, #0 is ridge, 1 is lasso
-               lambda=grid)
-coef(ridge2)
-
-#Put coefficients in a data frame, except the intercept
-coefs_ridge<-data.frame(t(as.matrix(coef(ridge2)))) %>% select(-X.Intercept.)
-#add the lambda grid to to data frame
-coefs_ridge<- coefs_ridge %>% mutate(lambda=grid)              
-
-#ggplot friendly format
-coefs_ridge<- coefs_ridge %>% pivot_longer(cols=!lambda,
-                                           names_to="variables",
-                                           values_to="coefficients")
-
-
-
-ggplot(data=coefs_ridge, aes(x = lambda, y = coefficients, color = variables)) +
-  geom_line() +
-  scale_x_log10(
-    breaks = scales::trans_breaks("log10", function(x) 10^x),
-    labels = scales::trans_format("log10",
-                                  scales::math_format(10^.x))
-  ) +
-  labs(title = "Coeficientes Ridge", x = "Lambda", y = "Coeficientes") +
-  theme_bw() +
-  theme(legend.position="bottom")
-
-#1 - Lasso 2 ---------------------------------------------------------
-
-modelo_lasso <- glmnet(
-  x = X,
-  y = y,
-  alpha = 1,
-  nlambda = 300,
-  standardize = FALSE
-)
-
-modelo_lasso
+coef_ridge_p3 <- coef(ridge_p3$finalModel, ridge_p3$bestTune$lambda)
+coef_ridge_p3
